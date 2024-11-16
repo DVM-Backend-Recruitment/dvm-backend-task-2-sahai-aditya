226c5cbf58088371ff790af8972483908ef82d2d
Aditya Sahai <sahai.aditya.0204@gmail.com>
2024-11-17 00:16:39 +0530

    Making shows model

--- shows/admin.py
+++ shows/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Theatre, TheatreAdmin, Movie
+from .models import Theatre, TheatreAdmin, Movie, Show
 
 admin.site.register(Theatre)
 admin.site.register(TheatreAdmin)
 admin.site.register(Movie)
+admin.site.register(Show)
--- shows/models.py
+++ shows/models.py
@@ -16,3 +16,13 @@ class Movie(models.Model):
     name = models.CharField(max_length=100)
     genre = models.CharField(max_length=30)
     image = models.ImageField(upload_to="movie-images/")
+
+    def __str__(self):
+        return self.name
+
+class Show(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    theatre = models.ForeignKey(Theatre, on_delete=models.CASCADE)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    vacant_seats = models.SmallIntegerField()
